6ab3498e539c150ec91687c7e60e8d57d38641b6
Rich Wareham <rich@richwareham.com>
2021-11-09 20:25:37 +0000

    api:db: add functions to create and retrieve auth tokens
    Add some simple functions to associate tokens with users and retrieve a
    user from the token. Will be used by a custom auth middleware.

--- src/api/db.ts
+++ src/api/db.ts
@@ -1,5 +1,6 @@
 import Knex from "knex";
 import path from "path";
+import { randomUUID } from "crypto";
 
 const knex = Knex({
   client: "sqlite3",
@@ -60,6 +61,45 @@ export async function getSubmissionAndUser(id: string): Promise<Submission> {
       .leftJoin("users", "users.id", "=", "submissions.user_id")
       .where("submissions.id", id)
   )[0];
+}
+
+// Create a new token for the user with the passed username. Returns undefined
+// if there is no such user. If a token already exists for the passed user, it
+// is returned.
+export async function createToken(username: string) {
+  const existingUser = await knex
+    .from("users")
+    .select("users.id", "tokens.token")
+    .leftJoin("tokens", "tokens.user_id", "=", "users.id")
+    .where("users.username", "=", username)
+    .first();
+
+  // If there is no user, return undefined.
+  if (!existingUser) {
+    return;
+  }
+
+  // If the user has a token, return it.
+  if (existingUser.token) {
+    return existingUser.token;
+  }
+
+  // Otherwise, create a new one.
+  const token = randomUUID();
+  await knex.table("tokens").insert({ token, user_id: existingUser.id });
+  return token;
+}
+
+// Given a token, return the matching user or undefined if there is no user with
+// a matching token.
+export async function userFromToken(token: string) {
+  const user: User | undefined = await knex
+    .table("users")
+    .first("username", "users.created_at", "users.id", "role_id", "description")
+    .leftJoin("tokens", "tokens.user_id", "=", "users.id")
+    .leftJoin("roles", "roles.id", "=", "users.role_id")
+    .where("tokens.token", token);
+  return user;
 }
 
 // Tear down any open database connections. Usually you will not need to call
