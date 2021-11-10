e23af87957cb099d4858a470c4dc289190a99079
Rich Wareham <rich@richwareham.com>
2021-11-10 17:17:57 +0000

    client:index: wire in react-query
    React Query maintains an application-wide cache. Add in wrapper to
    index.tsx.

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -2,6 +2,7 @@ import ReactDOM from "react-dom";
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { Box, CssBaseline, ThemeProvider } from "@mui/material";
+import { QueryClient, QueryClientProvider } from "react-query";
 import { Users } from "./Components/Users";
 import { Home } from "./Components/Home";
 import { Submissions } from "./Components/Submissions";
@@ -9,26 +10,30 @@ import { Header } from "./Components/Header";
 import { Navigation } from "./Components/Navigation";
 import { theme } from "./theme";
 
+const queryClient = new QueryClient();
+
 const App = () => {
   return (
     <Router>
       <ThemeProvider theme={theme}>
-        <Box sx={{ display: "flex" }}>
-          <CssBaseline />
-          <Header />
-          <Navigation />
-          <Switch>
-            <Route path="/users">
-              <Users />
-            </Route>
-            <Route path="/submissions">
-              <Submissions />
-            </Route>
-            <Route path="/">
-              <Home />
-            </Route>
-          </Switch>
-        </Box>
+        <QueryClientProvider client={queryClient}>
+          <Box sx={{ display: "flex" }}>
+            <CssBaseline />
+            <Header />
+            <Navigation />
+            <Switch>
+              <Route path="/users">
+                <Users />
+              </Route>
+              <Route path="/submissions">
+                <Submissions />
+              </Route>
+              <Route path="/">
+                <Home />
+              </Route>
+            </Switch>
+          </Box>
+        </QueryClientProvider>
       </ThemeProvider>
     </Router>
   );
