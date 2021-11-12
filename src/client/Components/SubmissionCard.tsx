6200ccc38618319473967d22c5b10acd5bd8a19b
Rich Wareham <rich@richwareham.com>
2021-11-12 08:17:12 +0000

    client:SubmissionCard: show a link to a submission location if provided
    _Technically_ this might end up leaking information on a source if
    someone is monitoring the Google Maps positions searched for by a
    journalist. If we're in the realm of state-sponsored surveillance then
    the submission app itself is probably being monitored via a stealth
    browser extension installed by the Milk Tray man.

--- src/client/Components/SubmissionCard.tsx
+++ src/client/Components/SubmissionCard.tsx
@@ -1,10 +1,16 @@
 import * as React from "react";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
+import Link from "@mui/material/Link";
 import Typography from "@mui/material/Typography";
 import { Submission } from "../model";
 import SubmissionCommentInput from "./SubmissionCommentInput";
 import SubmissionComments from "./SubmissionComments";
+
+const mapUrlFromLocation = (location: string) => {
+  const encodedLocation = encodeURIComponent(location);
+  return `https://www.google.com/maps/@?api=1&map_action=map&zoom=12&center=${encodedLocation}`;
+};
 
 export default function SubmissionCard(submission: Submission) {
   return (
@@ -14,6 +20,16 @@ export default function SubmissionCard(submission: Submission) {
         <p>{submission.text}</p>
         <p style={{ fontStyle: "italic" }}>{submission.username}</p>
         <p>{submission.created_at}</p>
+        {submission.location && (
+          <p>
+            <Link
+              href={mapUrlFromLocation(submission.location)}
+              target="_blank"
+            >
+              location
+            </Link>
+          </p>
+        )}
         {submission.comment_count === 0 ? (
           <Typography
             sx={{ color: "text.secondary" }}
