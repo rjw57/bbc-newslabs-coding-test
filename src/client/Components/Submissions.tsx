d8e94aa882c6f7b9f1260a66578153ceef9feec3
Rich Wareham <rich@richwareham.com>
2021-11-12 14:24:11 +0000

    client:Submissions: add ability to create new submissions
    Add the ability to create new submissions via the create submission
    dialog.

--- src/client/Components/Submissions.tsx
+++ src/client/Components/Submissions.tsx
@@ -1,11 +1,13 @@
-import React, { useEffect } from "react";
-import { Box, Grid, Toolbar, Typography } from "@mui/material";
+import React, { useEffect, useState } from "react";
+import { Box, Button, Grid, Toolbar, Typography } from "@mui/material";
 import { Submission } from "../model";
 import SubmissionCard from "./SubmissionCard";
+import CreateSubmissionDialog from "./CreateSubmissionDialog";
 import useAuthenticatedUser from "../hooks/useAuthenticatedUser";
 import useSubmissions from "../hooks/useSubmissions";
 
 export function Submissions() {
+  const [showCreateDialog, setShowCreateDialog] = useState(false);
   const { data: user } = useAuthenticatedUser();
   const { error, data: submissions, isLoading } = useSubmissions();
 
@@ -13,6 +15,10 @@ export function Submissions() {
   useEffect(() => {
     error && console.error(error);
   }, [error]);
+
+  const handleCreateDialogClose = () => {
+    setShowCreateDialog(false);
+  };
 
   function createSubmissionsCard(submission: Submission): JSX.Element {
     return (
@@ -24,6 +30,19 @@ export function Submissions() {
 
   return (
     <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+      <Toolbar /> {/* For spacing */}
+      {user?.description === "public" && (
+        <Box sx={{ pb: 2 }}>
+          <Button
+            variant="outlined"
+            onClick={() => {
+              setShowCreateDialog(true);
+            }}
+          >
+            Create new submission
+          </Button>
+        </Box>
+      )}
       {(!user || user.isAnonymous) && (
         <Typography sx={{ color: "error.main" }}>
           Sign in to view submissions.
@@ -36,6 +55,10 @@ export function Submissions() {
           (submissions || []).map(createSubmissionsCard)
         )}
       </Grid>
+      <CreateSubmissionDialog
+        open={showCreateDialog}
+        onClose={handleCreateDialogClose}
+      />
     </Box>
   );
 }
